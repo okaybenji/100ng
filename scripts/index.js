eb6c3ee4fa019dc412a7762b76572d1c7765f062
Benji Kay <okaybenji@gmail.com>
2016-07-01 16:10:51 -0500

    allow player to move within bounds via mouse

--- scripts/index.js
+++ scripts/index.js
@@ -5,10 +5,10 @@ game.appendChild(paddleContainer);
 
 // subtract container paddle dimensions to keep it from appearing partially off the court
 // position paddle container after it's added so it has dimensions to calculate from
-//paddleContainer.style.left = utils.randomIntBetween(0, game.clientWidth - paddleContainer.clientWidth);
-//paddleContainer.style.top = utils.randomIntBetween(0, game.clientHeight - paddleContainer.clientHeight);
-paddleContainer.style.left = game.clientWidth - paddleContainer.clientWidth;
-paddleContainer.style.top = game.clientHeight - paddleContainer.clientHeight;
+paddleContainer.style.left = utils.randomIntBetween(0, game.offsetWidth - paddleContainer.offsetWidth);
+paddleContainer.style.top = utils.randomIntBetween(0, game.offsetHeight - paddleContainer.offsetHeight);
+//paddleContainer.style.left = game.offsetWidth - paddleContainer.offsetWidth;
+//paddleContainer.style.top = game.offsetHeight - paddleContainer.offsetHeight;
 
 const paddle = Paddle();
 paddleContainer.appendChild(paddle);
@@ -16,6 +16,20 @@ paddleContainer.appendChild(paddle);
 paddle.style.backgroundColor = utils.randomColor();
 paddleContainer.appendChild(paddle);
 
+let startY = 0;
+
+game.addEventListener('mousemove', function(event) {
+  let newY = event.clientY - startY;
+  const maxY = paddleContainer.offsetHeight - paddle.offsetHeight;
+  newY = newY < 0 ? 0 : newY > maxY ? maxY : newY;
+  paddle.style.top = newY;
+
+  if (newY === 0) {
+    startY = event.clientY;
+  } else if (newY === maxY) {
+    startY = event.clientY - maxY;
+  }
+});
 /*game.onmousemove(function(event) {
   
 });*/
